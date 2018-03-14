1269f0df01d9bf608acdc18eceb6918e2381a5f8
taonlyt <taonlyt@aliyun.com>
2018-03-14 23:20:15 +0800

    change style

--- app/src/main/java/com/ibamb/udm/instruct/impl/InstructFrameEncoder.java
+++ app/src/main/java/com/ibamb/udm/instruct/impl/InstructFrameEncoder.java
@@ -1,9 +1,7 @@
 package com.ibamb.udm.instruct.impl;
 
 import com.ibamb.udm.instruct.IEncoder;
-import com.ibamb.udm.instruct.IParser;
 import com.ibamb.udm.instruct.beans.InstructFrame;
-import com.ibamb.udm.instruct.beans.ReplyFrame;
 
 /**
  * Created by luotao on 18-3-14.
